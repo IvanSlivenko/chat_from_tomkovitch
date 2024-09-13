46587d9ef808913dfcc5ecd66304f681c2ff3876
IvanSlivenko <ivan8822@ukr.net>
2024-09-13 21:41:40 +0300

    13-09-2024-21-41

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,8 +1,93 @@
 import React from 'react'
+import { useState } from 'react';
+import styles from '../styles/Main.module.css';
+import { Link } from 'react-router-dom'
+
+const FIELDS ={
+  NAME: "name",
+  LASTNAME: "lastname",
+  ROOM: "room"
+
+}
 
 const Main = () => {
+
+  const {NAME, LASTNAME, ROOM }  = FIELDS;
+  const [values, setValues] = useState( {[NAME]: "", [LASTNAME]: "", [ROOM]: ""} );
+
+  const handleChange =({ target: { name, value,}})=>{
+    setValues({...values, [name]: value })
+  }
+
+  const handleClick =(e)=>{
+    const isDisabled = Object.values(values).some((v) => !v);
+
+    if(isDisabled) e.preventDefault()
+
+  }
+  
   return (
-    <div>Main</div>
+   <div className={styles.wrap}>
+    <div className={styles.container}>
+    <h1 className={styles.heading}>Приєднатись</h1>
+    <form
+      className={styles.form} 
+    >
+      <div className={styles.group}>
+        <input 
+          type="text" 
+          name='name' 
+          value={values[NAME]}
+          placeholder="Ім'я"  
+          className={styles.input}  
+          onChange={handleChange}
+          autoComplete='off'
+          required
+        />
+      </div>
+
+      <div className={styles.group}>
+        <input 
+          type="text" 
+          name='lastname' 
+          value={values[LASTNAME]} 
+          placeholder="Прізвище "
+          className={styles.input}  
+          onChange={handleChange}
+          autoComplete='off'
+          required  
+        />
+      </div>
+
+      <div className={styles.group}>
+        <input 
+          type="text" 
+          name='room' 
+          value={values[ROOM]} 
+          placeholder="Назва чату "
+          className={styles.input}  
+          onChange={handleChange}
+          autoComplete='off'
+          required  
+        />
+      </div>
+
+      
+      <Link 
+  className={styles.group}
+  to={`/chat?name=${values[NAME]}&room=${values[ROOM]}&lastname=${values[LASTNAME]}`}
+  onClick={handleClick}
+>
+  <button 
+    type='submit'
+    className={styles.button}
+  >
+    Sign In
+  </button>
+</Link>
+    </form>
+    </div>
+   </div> 
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './components/App';
+import  './styles/main.css';
+
 
 
 const container =  document.getElementById('root');
